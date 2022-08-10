7c968279c82ebc4c63de14bf5200a03be9945ee7
Yesenia Motola <yeseniamotola@gmail.com>
2022-08-10 09:57:28 -0300

    correcciones

--- script.js
+++ script.js
@@ -7,23 +7,47 @@ class MichiBall{
         this.vida = vida
       }
 
-      comunicarse = () => console.log(`${this.etnia} te dice "miau" `);
-      pedirAbrazo = () => console.log(`${this.etnia} necesita un abrazo o morirá :( `);
+      comunicarse = () => alert(`${this.etnia} te dice "miau" `);
+      pedirAbrazo = () => alert(`${this.etnia} necesita un abrazo o morirá :( `);
   } 
 
 
-  const michiBall1 = new MichiBall("michi fresita", 1000, "Fresita", 5000);
-  const michiBall2 = new MichiBall("michi mandarinita", 1300, "Mandarinita", 6000);
-  const michiBall3 = new MichiBall("michi naranjita", 2000, "Naranjita", );
-  const michiBall4 = new MichiBall("michi arandanito", 3000, "Arandanito");
-  const michiBall5 = new MichiBall("michi aguacatito", 2000000, "Aguacatito");
+  const michiBall1 = new MichiBall("michi fresita", 1000, "Fresa", 5000);
+  const michiBall2 = new MichiBall("michi mandarinita", 1300, "Mandarina", 6000);
+  const michiBall3 = new MichiBall("michi naranjita", 2000, "Naranja", 5500);
+  const michiBall4 = new MichiBall("michi arandanito", 3000, "Arándano", 5800);
+  const michiBall5 = new MichiBall("michi aguacatito", 2000000, "Aguacate", 6800);
   
- console.log(michiBall1, michiBall2, michiBall3, michiBall4, michiBall5)
-  
-  michiBall3.comunicarse()
-  michiBall4.pedirAbrazo()
-  michiBall2.comunicarse()
-  michiBall5.pedirAbrazo()
+ const michiballs = [michiBall1, michiBall2, michiBall3, michiBall4, michiBall5]
+ const etniasMichiballs = [" Michi Fresita", " Michi Mandarinita", " Michi Naranjita", " Michi Arandanito", " Michi Aguacatito"]
 
-  
-  
+  console.table(michiballs)
+
+  alert(`Estas son las etnias conocidas de la raza alienígena "Michiballs": ${etniasMichiballs}`)
+
+
+let michiElegido = prompt("¿Con que MichiBall deseas interactuar? (Escribir la palabra Michi seguida de la etnia, a los michiballs les encanta usar diminutivos, recuerda usarlos también para referirte a ellos).").toLowerCase
+
+if (michiElegido = "michi fresita") {
+    alert(`Has elegido a Michi Fresita, ${michiBall1.comunicarse()}`)
+} else if (michiElegido = "michi mandarinita") {
+    alert(`Has elegido a Michi Mandarinita, ${michiBall2.comunicarse()}`)
+} else if (michiElegido = "michi naranjita") {
+    alert(`Has elegido a Michi Naranjita, ${michiBall3.comunicarse()}`)
+} else if (michiElegido = "michi arandanito") {
+    alert(`Has elegido a Michi Arandanito, ${michiBall4.comunicarse()}`)
+} else if (michiElegido = "michi aguacatito") {
+    alert(`Has elegido a Michi Aguacatito, ${michiBall5.comunicarse()}`)
+} else {
+    alert("Ingresa una opción válida")
+}
+
+alert(`Queremos que sepas que ${michiBall3.pedirAbrazo}`) 
+
+let respuesta = prompt("¿Deseas abrazar a michi Naranjita?")
+
+if (respuesta === "si") {
+    alert("Has abrazado a tu michiBall, ahora vivirá una vida larga <3")
+} else {
+    alert("Tu michiBall murió :( ")
+}
